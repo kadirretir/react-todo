177ea6c3a5d6caf6a0f9ebf7851d21862172392b
Kadir <kadirramazan344@gmail.com>
2021-06-06 08:48:19 +0300

    React ToDo app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [ToDos, setToDos] = useState("");
+  const [InToDo, InsetToDos] = useState([]);
+
+  const getInputValue = (e) => {
+    setToDos(e.target.value);
+  }
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    if(ToDos === "") {
+      alert("BOŞ GİRİLDİ");
+    } else {
+      InsetToDos([...InToDo, { text: ToDos, id: Math.random() * 300 }]);
+      setToDos("");
+    }
+  
+  }
+
+  const deleteToDo = (id) => {
+    const newList = InToDo.filter((ToDo) => id !== ToDo.id);
+    InsetToDos(newList);
+  }
+
+  function ToDoComp() {
+    return (
+      <>
+        {(InToDo.length > 0) ? InToDo.map(ToDo => (
+          <li key={ToDo.id}><b>{ToDo.text}</b>
+            <button onClick={() => deleteToDo(ToDo.id)} className="decrease btn">-</button>
+          </li>
+        )) : "Yapılacaklar listeniz boş..."}
+      </>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>YAPILACAKLAR LİSTESİ</h1>
+        <div className="todo">
+
+          <div className="add_input_div">
+            <form>
+              <input value={ToDos} type="text" onChange={getInputValue} placeholder="Yapılacak bir şey girin..." />
+              <button onClick={formHandler} className="increase btn">+</button>
+            </form>
+          </div>
+
+          <div className="todo_list">
+            <ol>
+              <ToDoComp />
+            </ol>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
